3ff21026786511a23860a74b86e75014764772b7
Patuco <patricia.beco.dg@gmail.com>
2023-08-28 18:13:06 +0200

    add header structure and styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 const Header = () => {
-    return(
-        <p>esto es el header</p>
-    )
-}
+  return (
+    <header className="header">
+      <h2 className="header__title">Mesa para Dos</h2>
+      <h3 className="header__subtitle">
+        Busca y reserva en tus restaurantes favoritos
+      </h3>
+      <label className="header__finder" htmlFor="search">
+        <i class="header__finder--icon fa-solid fa-magnifying-glass"></i>
+        <input
+        className="header__finder--input"
+          type="text"
+          placeholder="¿Dónde quieres comer hoy?"
+         />
+      </label>
+    </header>
+  );
+};
 
 export default Header;
